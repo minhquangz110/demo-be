4b4c1fb14e53f3d916f3ee01eead7cf9f4cff4a4
Bestcoc <concockun@gmail.com>
2022-11-09 08:46:32 +0700

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {
+    setInterval(() => {
+      this.pingHeroku();
+    }, 500000);
+  }
+
+  pingHeroku() {
+    const api = this.httpService.get(
+      `https://demo-nestjs-z.herokuapp.com/products`,
+    );
+    return lastValueFrom(api);
   }
 }
