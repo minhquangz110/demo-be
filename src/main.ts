565e2574ae53eeae51b0cd6282f6a393a866381b
Bestcoc <concockun@gmail.com>
2023-01-10 17:54:56 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const cors = require('cors');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableCors();
-  app.use(cors());
+  // app.setGlobalPrefix('api');
+  // app.use(
+  //   cors({
+  //     origin: '*',
+  //     method: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+
+  //     credentials: true,
+  //   }),
+  // );
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  });
+
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
